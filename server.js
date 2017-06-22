c087fd9097c18c8f9f1c31a103524e0391863f91
Chirag Sane <chiragsane333@gmail.com>
2017-06-22 15:05:22 +0530

    split users route

--- server.js
+++ server.js
@@ -8,72 +8,10 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var router = express.Router();
-
-var User = require('./models/user');
-
-router.use(function(req, res, next) {
-    console.log('Got a request!');
-    next();
-});
-
-router.get('/', function(req, res) {
+app.get('/api', function(req, res) {
     res.json({ message: 'API connected sucessfully!' });
 });
-
-router.route('/users')
-    .post(function(req, res) {
-        var user = new User();
-        user.name = req.body.name;
-        user.username = req.body.username;
-        user.password = req.body.password;
-        user.save(function(err) {
-            if (err)
-                res.send(err);
-            res.json({ message: 'User created!' });
-        });
-    })
-    .get(function(req, res) {
-        User.find(function(err, users) {
-            if (err)
-                res.send(err);
-            res.json(users);
-        });
-    });
-
-router.route('/users/:user_id')
-    .get(function(req, res) {
-        User.findById(req.params.user_id, function(err, user) {
-            if (err)
-                res.send(err);
-            res.json(user);
-        });
-    })
-    .put(function(req, res) {
-        User.findById(req.params.user_id, function(err, user) {
-            if (err)
-                res.send(err);
-            user.name = req.body.name;
-            user.username = req.body.username;
-            user.password = req.body.password;
-            user.save(function(err) {
-                if (err)
-                    res.send(err);
-                res.json({ message: 'User updated!' });
-            });
-        });
-    })
-    .delete(function(req, res) {
-        User.remove({
-            _id: req.params.user_id
-        }, function(err, user) {
-            if (err)
-                res.send(err);
-            res.json({ message: 'Successfully deleted' });
-        });
-    });
-
-app.use('/api', router);
+app.use('/api/users', require('./routes/users'));
 
 var port = process.env.PORT || 8080;
 
